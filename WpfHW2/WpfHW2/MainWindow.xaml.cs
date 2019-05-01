b0f783028143653f9e10d7af5d9d7436c5022f52
Aseka <grooves97@gmail.com>
2019-05-02 00:36:45 +0600

    Поставщик Apixu | Создал пробное подключение к api

--- WpfHW2/WpfHW2/MainWindow.xaml.cs
+++ WpfHW2/WpfHW2/MainWindow.xaml.cs
@@ -22,9 +22,22 @@ namespace WpfHW2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string url = "http://api.apixu.com/v1/current.json?key=d05add50a1364f23a5b182422190105&q=Astana";
         public MainWindow()
         {
             InitializeComponent();
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader=new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
         }
+
     }
 }
